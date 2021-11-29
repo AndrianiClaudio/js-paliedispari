467623a992ce15ab23e9590763cde018b4b558c2
AndrianiClaudio <claudioandriani1994@gmail.como>
2021-11-29 15:01:00 +0100

    ciclo while: controllo solo lettere necessarie

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -4,6 +4,13 @@
 */
 // funzione: word --> true: parola palindrona;false: non polindroma
 function isPalindroma(word) {
+    // si scorrerá fino a metá parola. (se dispari la metá si arrotonda per difetto. la letterá a metá parola rimane a metá parola al contrario).
+    // console.log(word.length);
+    if((word.length) % 2 == 0) { //lughezza parola pari
+        indexMax = word.length / 2;
+    } else { //lughezza parola dispari
+        indexMax = Math.floor(word.length / 2);
+    }
     let check = Boolean();
     let i = 0;
     do {
@@ -14,14 +21,13 @@ function isPalindroma(word) {
             check = false;
         }
         i++;
-    } while (i < word.length && check);
+    } while (i < indexMax && check);
     return check;
 }
 //  Richiedo una parola all'utente
 const word = prompt('Inserisci una parola').toLowerCase();
-//  Controllo se la parola é uguale a se stessa al contrario tramite funzione:true: parola palindrona;false: non polindroma
-const check = isPalindroma(word);
-if (check) {
+//  Controllo se la parola é uguale a se stessa al contrario tramite funzione
+if (isPalindroma(word)) {
     console.log('La parola é polindroma');
 } else {
     console.log('La parola NON é polindroma');
